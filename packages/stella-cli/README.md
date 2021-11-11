35e08a7c5dd6bb5b05312b8b5e025596a1cd19df
SHUAXINDIARY <shuaxinjs@qq.com>
2021-11-11 13:12:51 +0800

    feat: 判断init目录是否为空

---
+++ packages/stella-cli/README.md
@@ -0,0 +1,22 @@
+# stella-cli
+
+- 快捷生成一个使用了stella主题的vuepress2.x目录
+
+# 使用
+
+```shell
+
+# 安装
+npm i -g stella-cli
+
+# 初始化
+
+stella-cli -init stella-demo
+
+# 安装依赖
+
+cd stella-demo
+
+npm i # 或者 yarn i
+
+```
---
+++ packages/stella-cli/bin/stella-cli.ts
@@ -0,0 +1,114 @@
+#!/usr/bin/env node
+
+const path = require("path");
+// @ts-ignore
+const process = require("process");
+const readline = require("readline");
+const fs = require("fs");
+const { createCommand } = require("commander");
+const program = createCommand();
+const package = require("../package.json");
+const chalk = require("chalk");
+
+const command = {
+  i: "initTemplate",
+  git: "git",
+};
+
+const commandMap = {
+  // 生成模板
+  [command.i]: initTemplate,
+  [command.git]: addGitignore,
+};
+
+const _exit = process.exit;
+const _argv = process.argv;
+
+program
+  .name(chalk.bgBlue(chalk.white("stella")))
+  .description(chalk.bgBlue(chalk.white("vuepress2.x theme cli")))
+  .version(package.version)
+  .option(
+    `-i,   --${command.i}`,
+    chalk.bgMagenta(
+      chalk.white("generator a vuepress using Stella theme directory")
+    )
+  )
+  .option(
+    `-git, --${command.git}`,
+    chalk.bgGreen(chalk.white("add .gitignore"))
+  )
+  .parse(_argv);
+
+if (!_exit.exited) {
+  main();
+}
+
+function main() {
+  const opts = program.opts();
+  const shellOpts = Object.keys(opts);
+  if (shellOpts.length) {
+    start(shellOpts);
+  } else {
+    program.outputHelp();
+  }
+}
+
+function start(opts) {
+  opts.forEach(opt => {
+    commandMap[opt]();
+  });
+}
+
+function initTemplate() {
+  const destinationPath = program.args.shift() || ".";
+  const docName = createAppName(destinationPath) || "hello-stella";
+  console.log("初始化模板", docName);
+  emptyDirectory(destinationPath, function (empty) {
+    if (empty || program.force) {
+      createApplication(docName, destinationPath);
+    } else {
+      confirm("destination is not empty, continue? [y/N] ", function (ok) {
+        if (ok) {
+          process.stdin.destroy();
+          createApplication(docName, destinationPath);
+        } else {
+          console.error("aborting");
+        }
+      });
+    }
+  });
+}
+
+function addGitignore() {
+  console.log("添加gitignore");
+}
+
+function createAppName(pathName) {
+  return path
+    .basename(pathName)
+    .replace(/[^A-Za-z0-9.-]+/g, "-")
+    .replace(/^[-_.]+|-+$/g, "")
+    .toLowerCase();
+}
+
+function createApplication(appName, dir) {}
+
+function emptyDirectory(dir, fn) {
+  fs.readdir(dir, function (err, files) {
+    if (err && err.code !== "ENOENT") throw err;
+    fn(!files || !files.length);
+  });
+}
+
+function confirm(msg, callback) {
+  var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question(msg, function (input) {
+    rl.close();
+    callback(/^y|yes|ok|true$/i.test(input));
+  });
+}
